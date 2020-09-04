6d9d50c8237e7c8892d5d0af565b823c0af9d026
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-09-04 17:07:27 -0500

    Updates

---
+++ precache-manifest.9986a835c87f9b9ccadab7cac58e195e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672d96fd9fb391e54baa5bf6233b7663",
+    "url": "/recipe-page/index.html"
+  },
+  {
+    "revision": "6feb7843f0f6df988d8b",
+    "url": "/recipe-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1820d0c0ef6b3cb82ab9",
+    "url": "/recipe-page/static/js/2.e6a1196a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-page/static/js/2.e6a1196a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6feb7843f0f6df988d8b",
+    "url": "/recipe-page/static/js/main.0bc3a4fc.chunk.js"
+  },
+  {
+    "revision": "9e35d253da64cfd89e7e",
+    "url": "/recipe-page/static/js/runtime-main.0cdc79e3.js"
+  },
+  {
+    "revision": "109049fd5a06a15202d8cdf6b0c08b62",
+    "url": "/recipe-page/static/media/photo1.109049fd.png"
+  }
+]);
